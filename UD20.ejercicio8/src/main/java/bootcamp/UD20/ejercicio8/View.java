17f63c65e015c2f59ae67c1e000cf3f6c44463d2
Palmira Romia <kguinera@hotmail.com>
2023-04-07 18:40:57 +0200

    corrección de pequeño error

--- UD20.ejercicio8/src/main/java/bootcamp/UD20/ejercicio8/View.java
+++ UD20.ejercicio8/src/main/java/bootcamp/UD20/ejercicio8/View.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -34,7 +35,7 @@ public class View extends JFrame {
 	private double dinero;
 
 	public View() {
-		setTitle("Calculadora cambio de monedas");
+		setTitle("Calculadora cambio de monedas avanzado");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 271);
 		contentPane = new JPanel();
@@ -113,7 +114,6 @@ public class View extends JFrame {
 				dinero = Double.parseDouble(textMoneda.getText());
 			}catch(NumberFormatException ex) {
 				JOptionPane.showMessageDialog(View.this, "Solo se pueden números enteros o decimales","Error", JOptionPane.ERROR_MESSAGE);
-			}finally {
 				//Si salta el error queremos que se borren los datos
 				textMoneda.setText("0.0");
 				lblFinal.setText("0.0");
