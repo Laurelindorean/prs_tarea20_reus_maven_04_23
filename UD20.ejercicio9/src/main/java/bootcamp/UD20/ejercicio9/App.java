2a69af7e21a3225da1217e217a7403d6b657195f
Palmira Romia <kguinera@hotmail.com>
2023-04-09 11:49:27 +0200

    subo version MemoryGame con imagenes y añado opcion en la App

--- UD20.ejercicio9/src/main/java/bootcamp/UD20/ejercicio9/App.java
+++ UD20.ejercicio9/src/main/java/bootcamp/UD20/ejercicio9/App.java
@@ -2,17 +2,40 @@ package bootcamp.UD20.ejercicio9;
 
 import java.awt.EventQueue;
 
+import javax.swing.JOptionPane;
+
 public class App {
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MemoryGame frame = new MemoryGame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+		String opcion = JOptionPane.showInputDialog("Que ejercicio quieres corregir? 1 - MemoryGame con colores \n2- MemoryGame con Imagenes");
+		switch(opcion){
+		case "1":
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						MemoryGame frame = new MemoryGame();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
-			}
-		});
+			});
+			
+			break;
+		case "2":
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						MemoryGameImages frame = new MemoryGameImages();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			break;
+			default:
+				JOptionPane.showMessageDialog(null, "Debes Introducir un 1 o un 2");		
+		}
+		
 	}
 }
--- UD20.ejercicio9/src/main/java/bootcamp/UD20/ejercicio9/MemoryGameImages.java
+++ UD20.ejercicio9/src/main/java/bootcamp/UD20/ejercicio9/MemoryGameImages.java
@@ -1,27 +1,43 @@
 package bootcamp.UD20.ejercicio9;
+/**
+ * @author Palmira
+ */
 
-import java.awt.Color;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.Timer;
 
-public class MemoryGame extends JFrame implements ActionListener {
 
-	/**
-	 * @author Palmira
-	 */
+//Versión de mi MemoryGame pero con imagenes en vez de colores
+
+public class MemoryGameImages extends JFrame implements ActionListener{
+
 	private static final long serialVersionUID = -5468290645557986484L;
 	private JPanel contentPane;
 	private ArrayList<JToggleButton> listaBotones = new ArrayList<>();
-	private Color[] colores = { Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.MAGENTA, Color.ORANGE,
-			Color.PINK, Color.CYAN };
+	ImageIcon imagen1 = new ImageIcon("src/main/java/images/1.png");
+	ImageIcon imagen2 = new ImageIcon("src/main/java/images/2.png");
+	ImageIcon imagen3 = new ImageIcon("src/main/java/images/3.png");
+	ImageIcon imagen4 = new ImageIcon("src/main/java/images/4.png");
+	ImageIcon imagen5 = new ImageIcon("src/main/java/images/5.png");
+	ImageIcon imagen6 = new ImageIcon("src/main/java/images/6.png");
+	ImageIcon imagen7 = new ImageIcon("src/main/java/images/7.png");
+	ImageIcon imagen8 = new ImageIcon("src/main/java/images/8.png");
+	ImageIcon image9 = new ImageIcon("src/main/java/images/questionMark.png");
+	
+	//Creamos un array de ImageIcon
+	private ImageIcon[] imagenes = { imagen1, imagen2, imagen3, imagen4, imagen5, imagen6,
+			imagen7, imagen8 };
 
 	private static int turno = 0;
 	private JToggleButton botonPrev = null;
@@ -29,37 +45,41 @@ public class MemoryGame extends JFrame implements ActionListener {
 	private Timer timer;
 	private int intentos;
 
-
 	/**
 	 * Create the frame.
 	 */
-	public MemoryGame() {
-		setTitle("Memory Game");
+	public MemoryGameImages() {
+		setTitle("Memory Game with Images");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(4, 4, 1, 1));
+		
 
-
-		for (Color color : colores) {
-			listaBotones.add(this.crearJToggleButton(color));
-			listaBotones.add(this.crearJToggleButton(color));
+		for (ImageIcon image : imagenes) {
+			//Rellenamos 2 veces la lista de botones con imagenes
+			listaBotones.add(this.crearJToggleButton(image));
+			listaBotones.add(this.crearJToggleButton(image));
+			
 		}
-
+		//Mezclamos las imagenes
 		Collections.shuffle(listaBotones);
 		for (JToggleButton button : listaBotones) {
 			contentPane.add(button);
+		
 		}
 	}
 
-	private JToggleButton crearJToggleButton(Color color) {
+	private JToggleButton crearJToggleButton(ImageIcon image) {
 		JToggleButton button = new JToggleButton();
-		button.setBackground(color);
+		
 		button.addActionListener(this);
-		button.setName(color.toString());
+		button.setName(image.toString());
+		button.setIcon(image);
 		button.setSelected(true);
-
+		//Indicamos que de fondo nos ponga otra imagen 
+		button.setSelectedIcon(image9);
 		return button;
 	}
 
@@ -76,10 +96,10 @@ public class MemoryGame extends JFrame implements ActionListener {
 			this.botonPost = (JToggleButton) e.getSource();
 			timer = new Timer(500, timerEvent);
 			intentos++;
-			
+			/*
 			for(JToggleButton button : listaBotones) {
 				button.setEnabled(false);
-			}
+			}*/
 			timer.setRepeats(false);
 			timer.start();
 			break;
@@ -96,7 +116,7 @@ public class MemoryGame extends JFrame implements ActionListener {
 	
 	private ActionListener timerEvent = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			if (botonPrev.getBackground().equals(botonPost.getBackground())) {
+			if (botonPrev.getIcon().equals(botonPost.getIcon())) {
 				botonPrev.setVisible(false);
 				botonPost.setVisible(false);
 			} else {
